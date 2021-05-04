0e5a2a485c09d799d303d64bb6e23bc1a322e53a
SeonJungKim <ksj083895@gmail.com>
2021-05-04 15:04:59 +0500

    Initial Commit

---
+++ SwiftUI_Pokemon/SwiftUI_PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PokemonApp.swift
+//  SwiftUI_Pokemon
+//
+//  Created by 김선중 on 2021/05/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
